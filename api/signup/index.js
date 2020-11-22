14eed8ff074e98d597df6563d0caff7bc8cd4e42
morgan4080 <morganmurungi@live.com>
2020-11-22 15:59:55 +0300

    jwt

--- api/signup/index.js
+++ api/signup/index.js
@@ -6,33 +6,9 @@ const prisma = new PrismaClient();
 
 const bcrypt = require('bcrypt');
 
-const jwt = require('jsonwebtoken');
-
-const { readFileSync } = require('fs');
-
-const { join } = require('path');
-
-const privateKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key'), 'utf8');
-
-/**
- * Store a newly created user.
- *
- * @return Object
- * @param req
- * @param res
- */
-
-async function store(req, res) {
-  try {
-    const clientEmail = req.body.email;
-
-    const user = await prisma.users.findOne({
-      where: {
-        email: clientEmail
-      }
-    });
-
-    if (user === null) {
+module.exports = async function (req, res) {
+  if (Object.keys(req.query).length === 0 && req.method === "POST" && Object.keys(req.body).length !== 0  && req.body.name !== undefined) {
+    try {
       const saltRounds = 10
       const yourPassword = req.body.password
       let hashedPassword = await bcrypt.hash(yourPassword, saltRounds);
@@ -57,73 +33,12 @@ async function store(req, res) {
 
       if (response === null) res.status(500);
 
-      await doLogin(req, res);
-
-    } else {
-      res.status(301).json({
-        data: {
-          status: 'user already exists',
-          token: null,
-          user: null,
-        }
-      });
-    }
-
-  } catch (e) {
-    res.status(500);
-  }
-}
-
-async function doLogin(req, res) {
-
-  try {
-    const clientEmail = req.body.email;
-
-    const user = await prisma.users.findOne({
-      where: {
-        email: clientEmail
-      }
-    });
-
-    if (user === null) {
-      res.status(401).json({
-        data: {
-          status: 'user not found',
-          token: null,
-          user: null,
-        }
-      });
-    }
-
-    let match = await bcrypt.compare(req.body.password, user.password);
-
-    if (match) {
-      const token = jwt.sign({ user: user }, privateKey, { algorithm: 'RS256' });
       res.status(200).json({
         data: {
           status: 'success',
-          token: token,
-          user: user,
-        }
-      });
-    } else {
-      res.status(401).json({
-        data: {
-          status: 'password/email dont match our record',
-          token: null,
-          user: null,
+          response: response
         }
       })
-    }
-  } catch (e) {
-    res.status(500);
-  }
-}
-
-module.exports = async function (req, res) {
-  if (Object.keys(req.query).length === 0 && req.method === "POST" && Object.keys(req.body).length !== 0  && req.body.name !== undefined) {
-    try {
-      await store(req, res);
     } catch (e) {
       res.status(500);
     }
