6432fc2a90c4487e10f85a4b6b692865badaec39
morgan4080 <morganmurungi@live.com>
2020-11-22 16:24:42 +0300

    jwt

--- api/signup/index.js
+++ api/signup/index.js
@@ -13,21 +13,18 @@ module.exports = async function (req, res) {
       const yourPassword = req.body.password
       let hashedPassword = await bcrypt.hash(yourPassword, saltRounds);
 
-      let data = {
-        account_id: req.body.account_id,
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-        phone: req.body.phone,
-        provider: (req.body.provider !== undefined && req.body.provider) ? req.body.provider : null,
-        provider_id: (req.body.provider_id !== undefined && req.body.provider_id) ? req.body.provider_id : null,
-        owner: (req.body.owner !== undefined && req.body.owner) ? req.body.owner : false,
-        metadata: (req.body.metadata !== undefined && req.body.metadata) ? JSON.stringify(req.body.metadata) : null
-      };
-
       let response = await prisma.users.create({
         data: {
-          ...data
+          account_id: req.body.account_id,
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+          phone: req.body.phone,
+          email_verified_at: null,
+          provider: (req.body.provider !== undefined && req.body.provider) ? req.body.provider : null,
+          provider_id: (req.body.provider_id !== undefined && req.body.provider_id) ? req.body.provider_id : null,
+          owner: (req.body.owner !== undefined && req.body.owner) ? req.body.owner : false,
+          metadata: (req.body.metadata !== undefined && req.body.metadata) ? JSON.stringify(req.body.metadata) : null
         }
       });
 
