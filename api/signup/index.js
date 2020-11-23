833969cfa9dc37ef5ff51b392d5b9236012404dd
morgan4080 <morganmurungi@live.com>
2020-11-23 15:05:18 +0300

    jwt

--- api/signup/index.js
+++ api/signup/index.js
@@ -29,16 +29,12 @@ async function doSignup(req) {
 
   let response = await prisma.users.create({
     data: {
-      account_id: req.body.account_id,
+      accounts: { connect: { id: req.body.account_id } },
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
       phone: req.body.phone,
-      email_verified_at: null,
-      provider: (req.body.provider !== undefined && req.body.provider) ? req.body.provider : null,
-      provider_id: (req.body.provider_id !== undefined && req.body.provider_id) ? req.body.provider_id : null,
-      owner: (req.body.owner !== undefined && req.body.owner) ? req.body.owner : false,
-      metadata: (req.body.metadata !== undefined && req.body.metadata) ? JSON.stringify(req.body.metadata) : null
+      owner: false,
     }
   })
 
