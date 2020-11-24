295208f084c24c8715fd1732f4e06d143507b9b8
morgan4080 <morganmurungi@live.com>
2020-11-24 13:00:45 +0300

    jwt

--- api/me/index.js
+++ api/me/index.js
@@ -1,4 +1,31 @@
-import authMiddleware from '../serverMiddleware/auth-middleware'
+const jwt = require('jsonwebtoken')
+
+const { readFileSync } = require('fs')
+
+const { join } = require('path')
+
+const privateKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key'), 'utf8')
+
+function authMiddleware(req) {
+  return new Promise((resolve, reject) => {
+    const header = req.headers['authorization'];
+
+    if (header === undefined) {
+      reject("header undefined")
+    }
+
+    const bearer = header.split(' ');
+
+    const token = bearer[1];
+
+    jwt.verify(token, privateKey, (err, user) => {
+      if (err) {
+        reject("jwt verification error", err)
+      }
+      resolve(user)
+    })
+  })
+}
 
 module.exports = async function (req, res) {
   authMiddleware(req).then(user => {
