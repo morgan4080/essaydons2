9aeadc68bef53993403472c237133ce7ef114091
morgan4080 <morganmurungi@live.com>
2020-11-24 12:03:40 +0300

    jwt

--- api/me/index.js
+++ api/me/index.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-const { readFileSync } = require('fs');
+const { readFileSync } = require('fs')
 
-const { join } = require('path');
+const { join } = require('path')
 
-const privateKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key'), 'utf8');
+const privateKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key'), 'utf8')
 
-module.exports = async function (req, res) {
+function initAuthMiddleware(req, res) {
   const header = req.headers['authorization'];
 
   if (header === undefined) {
-    res.status(403).json({
-      data: header
-    })
+    res.status(403)
   }
 
   const bearer = header.split(' ');
@@ -23,13 +21,15 @@ module.exports = async function (req, res) {
 
   jwt.verify(token, privateKey, (err, user) => {
     if (err) {
-      res.status(403).json({
-        data: err
-      })
+      res.status(403)
     }
-    res.status(200).json({
-      data: user
-    })
+    req.user = user
   })
+}
 
-};
+module.exports = async function (req, res) {
+  await initAuthMiddleware(req, res)
+  res.status(200).json({
+    data: req.user
+  })
+}
