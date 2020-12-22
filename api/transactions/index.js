1490d66faf6ed01ccdd9cf5389ce30e834924e5a
morgan4080 <morganmurungi@live.com>
2020-12-23 01:09:56 +0300

    stripe

--- api/transactions/index.js
+++ api/transactions/index.js
@@ -1,18 +1,26 @@
 import stripe from "stripe"
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const { readFileSync } = require('fs')
+const { readFileSync } = require('fs');
 
-const { join } = require('path')
+const { join } = require('path');
 
-const publicKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key.pub'), 'utf8')
+const publicKey = readFileSync(join(__dirname, '../_JWTKeys', 'jwtRS256.key.pub'), 'utf8');
+
+const Stripe = require("stripe");
+
+const stripe = Stripe('sk_test_MKa3c0iWsVFbV9WK64cNnyQd');
+
+// Create your webhook in the Stripe dashboard at https://dashboard.stripe.com/webhooks
+// Use the secret listed in the "Signing secret" section
+const endpointSecret = 'whsec_pAgBXyncG361YrZ6Nq2f29EssTJsZDko';
 
 function authMiddleware(req) {
   return new Promise((resolve, reject) => {
@@ -35,7 +43,7 @@ function authMiddleware(req) {
   })
 }
 
-module.exports = async function (req, res) {
+module.exports = async (req, res) => {
 
     if (req.query.payment_intent && (req.body && req.body.order)) {
 
@@ -77,6 +85,49 @@ module.exports = async function (req, res) {
         });
       }
 
+    } else if (req.query.payment_succeeded && (req.body && req.body.order)) {
+      // payment_succeeded
+
+      // Webhook that listens for events sent from Stripe
+      // Requires configuration in the Stripe Dashboard
+      // For more information read https://stripe.com/docs/webhooks
+
+      const sig = req.headers["stripe-signature"];
+      let stripeEvent;
+
+      try {
+        // Verifies that the event was sent by Stripe and deserializes the event
+        stripeEvent = stripe.webhooks.constructEvent(
+          req.body,
+          sig,
+          endpointSecret
+        );
+      } catch (err) {
+        return { statusCode: 400 };
+      }
+
+      // Handle the event
+      switch (stripeEvent.type) {
+        case "payment_intent.succeeded":
+          const paymentIntent = stripeEvent.data.object;
+          console.log('object', paymentIntent)
+          console.log(
+            "Payment was successful! Charge information:",
+            paymentIntent.charges.data.filter(charge => charge.status === "succeeded")
+          );
+          break;
+        case "charge.dispute.created":
+          const charge = stripeEvent.data.object;
+          console.log("Someone disputed a payment!");
+          break;
+        // ... handle other event types
+        default:
+          // Unexpected event type
+          res.status(400)
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      res.status(200)
     } else {
 
       res.status(400).json({
--- pages/order.vue
+++ pages/order.vue
@@ -487,16 +487,20 @@
                     class="font-mono mt-2 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="card"
                     :class="{ complete }"
-                    stripe="pk_test_8ssZgwB2PiH0ajJksD2gVbsG00u7Y3IDPv"
+                    stripe="pk_test_poS6oPZNGfTxtuUFNgr4aG2f"
                     :options="stripeOptions"
                     @change="complete = $event.complete"
                   />
                   <small class="text-red-600">{{error}}</small>
                   <button
-                    class="mt-8 bg-black hover:bg-teal-300 text-white hover:text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform hover:scale-105 transition ease-in-out duration-100"
+                    class="flex items-center mt-8 bg-black hover:bg-teal-300 text-white hover:text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform hover:scale-105 transition ease-in-out duration-100"
                     @click="pay"
                     :disabled="!complete || !stripeEmail || loading"
                   >
+                    <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
                     Pay with credit card
                   </button>
                 </div>
@@ -954,7 +958,7 @@ export default {
         }));
       }
       let order = {
-        amount: this.totalPrice,
+        amount: Math.round((this.totalPrice + Number.EPSILON) * 100) / 100,
         ...this.form
       };
       console.log(order);
