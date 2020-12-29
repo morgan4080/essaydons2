904ffdda1450cc6e20f1d615a0e01c2665e2217a
morgan4080 <morganmurungi@live.com>
2020-12-29 08:08:43 +0300

    order mails

--- api/transactions/index.js
+++ api/transactions/index.js
@@ -17,6 +17,28 @@ const stripe = Stripe('sk_test_MKa3c0iWsVFbV9WK64cNnyQd');
 // Use the secret listed in the "Signing secret" section
 const endpointSecret = 'whsec_pAgBXyncG361YrZ6Nq2f29EssTJsZDko';
 
+const nodemailer = require("nodemailer");
+
+async function sendMail(origin,destination,message, attachments,auth = {user: "info@essaydons.co", pass: "Motsae254"}) {
+  let transporter = nodemailer.createTransport({
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: auth.user,
+      pass: auth.pass,
+    },
+  });
+  return await transporter.sendMail({
+    from: `"${origin.name}" <${origin.email}>`, // sender address
+    to: `${destination.email}`, // list of receivers
+    subject: `${message.subject}`, // Subject line
+    text: `${message.text}`, // plain text body
+    html: `${message.html}`, // html body
+    attachments: [...attachments],
+  });
+}
+
 function authMiddleware(req) {
   return new Promise((resolve, reject) => {
     const header = req.headers['authorization']
@@ -142,6 +164,293 @@ module.exports = async (req, res) => {
     }
 
     try {
+      let attachments = [];
+
+      const origin = {
+        name: "EssayDons Signups",
+        email: "info@essaydons.co",
+      };
+
+      const destination = {
+        email: user.email,
+      };
+
+      const message = {
+        subject: "Order Confirmed",
+        text: "Hey" + user.name,
+        html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Portfolio - Responsive Email Template</title>
+<style type="text/css">
+/* ----- Custom Font Import ----- */
+@import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin,latin-ext);
+
+/* ----- Text Styles ----- */
+table{
+font-family: 'Lato', Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-font-smoothing: antialiased;
+font-smoothing: antialiased;
+}
+
+@media only screen and (max-width: 700px){
+/* ----- Base styles ----- */
+.full-width-container{
+padding: 0 !important;
+}
+
+.container{
+width: 100% !important;
+}
+
+/* ----- Header ----- */
+.header td{
+padding: 30px 15px 30px 15px !important;
+}
+
+/* ----- Projects list ----- */
+.projects-list{
+display: block !important;
+}
+
+.projects-list tr{
+display: block !important;
+}
+
+.projects-list td{
+display: block !important;
+}
+
+.projects-list tbody{
+display: block !important;
+}
+
+.projects-list img{
+margin: 0 auto 25px auto;
+}
+
+/* ----- Half block ----- */
+.half-block{
+display: block !important;
+}
+
+.half-block tr{
+display: block !important;
+}
+
+.half-block td{
+display: block !important;
+}
+
+.half-block__image{
+width: 100% !important;
+background-size: cover;
+}
+
+.half-block__content{
+width: 100% !important;
+box-sizing: border-box;
+padding: 25px 15px 25px 15px !important;
+}
+
+/* ----- Hero subheader ----- */
+.hero-subheader__title{
+padding: 80px 15px 15px 15px !important;
+font-size: 35px !important;
+}
+
+.hero-subheader__content{
+padding: 0 15px 90px 15px !important;
+}
+
+/* ----- Title block ----- */
+.title-block{
+padding: 0 15px 0 15px;
+}
+
+/* ----- Paragraph block ----- */
+.paragraph-block__content{
+padding: 25px 15px 18px 15px !important;
+}
+
+/* ----- Info bullets ----- */
+.info-bullets{
+display: block !important;
+}
+
+.info-bullets tr{
+display: block !important;
+}
+
+.info-bullets td{
+display: block !important;
+}
+
+.info-bullets tbody{
+display: block;
+}
+
+.info-bullets__icon{
+text-align: center;
+padding: 0 0 15px 0 !important;
+}
+
+.info-bullets__content{
+text-align: center;
+}
+
+.info-bullets__block{
+padding: 25px !important;
+}
+
+/* ----- CTA block ----- */
+.cta-block__title{
+padding: 35px 15px 0 15px !important;
+}
+
+.cta-block__content{
+padding: 20px 15px 27px 15px !important;
+}
+
+.cta-block__button{
+padding: 0 15px 0 15px !important;
+}
+}
+</style>
+
+<!--[if gte mso 9]><xml>
+<o:OfficeDocumentSettings>
+<o:AllowPNG/>
+<o:PixelsPerInch>96</o:PixelsPerInch>
+</o:OfficeDocumentSettings>
+</xml><![endif]-->
+</head>
+
+<body style="padding: 0; margin: 0;" bgcolor="#eeeeee">
+<span style="color:transparent !important; overflow:hidden !important; display:none !important; line-height:0px !important; height:0 !important; opacity:0 !important; visibility:hidden !important; width:0 !important; mso-hide:all;">This is your preheader text for this email (Read more about email preheaders here - https://goo.gl/e60hyK)</span>
+
+<!-- / Full width container -->
+<table class="full-width-container" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" bgcolor="#eeeeee" style="width: 100%; height: 100%; padding: 30px 0 30px 0;">
+<tbody><tr>
+<td align="center" valign="top">
+<!-- / 700px container -->
+<table class="container" border="0" cellpadding="0" cellspacing="0" width="700" bgcolor="#ffffff" style="width: 700px;">
+<tbody><tr>
+<td align="center" valign="top">
+<!-- / Hero subheader -->
+<table class="container hero-subheader" border="0" cellpadding="0" cellspacing="0" width="620" style="width: 620px;">
+<tbody><tr>
+<td class="hero-subheader__title" style="font-size: 43px; font-weight: bold; padding: 80px 0 15px 0;" align="left">Hey <span style="color: #42fbb7;">${user.name}</span> you have made an order of ${req.body.order.amount} USD to Essaydons.co</td>
+</tr>
+
+<tr>
+<td class="hero-subheader__content" style="font-size: 15px; line-height: 27px; padding: 0 60px 90px 0;" align="left">
+  <table border="1" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Payment Method</p>
+            </td>
+            <td>
+                <p style="margin: 0;">Stripe</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Order ID</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${response.id}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Subject</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${req.body.order.subject}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Topic</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${req.body.order.topic}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Order Description</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${req.body.order.details}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;"> Pages - Spacing - Format</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${req.body.order.pages} - ${req.body.order.spacing} - ${req.body.order.format}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p style="margin: 0;font-weight: bold;">Total</p>
+            </td>
+            <td>
+                <p style="margin: 0;">${req.body.order.amount}</p>
+            </td>
+        </tr>
+    </table>
+</td>
+</tr>
+
+<tr>
+<td class="hero-subheader__content" style="font-size: 16px; line-height: 27px; padding: 0 60px 90px 0;" align="left">
+<a href="https://essaydons.co/profile" target="_blank" rel="noopener noreferrer" style="padding: 10px 20px 10px 20px; background-color: #42fbb7; color: white;">Order Details</a>
+</td>
+</tr>
+</tbody></table>
+<!-- / Footer -->
+<table class="container" border="0" cellpadding="0" cellspacing="0" width="100%" align="center">
+<tbody><tr>
+<td align="center">
+<table class="container" border="0" cellpadding="0" cellspacing="0" width="620" align="center" style="border-top: 1px solid #eeeeee; width: 620px;">
+<tbody><tr>
+<td style="text-align: center; padding: 50px 0 10px 0;">
+<a href="#" style="font-size: 28px; text-decoration: none; color: #d5d5d5;">Thank You</a>
+</td>
+</tr>
+
+<tr>
+<td style="color: #d5d5d5; text-align: center; font-size: 15px; padding: 10px 0 60px 0; line-height: 22px;">Copyright © 2021 <a href="https://essadons.co/" target="_blank" style="text-decoration: none; border-bottom: 1px solid #d5d5d5; color: #d5d5d5;">Essaydons</a>. <br>All rights reserved.</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+<!-- /// Footer -->
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+
+</body></html>`,
+      };
+
+      await sendMail(origin,destination,message,attachments)
+    } catch (e) {
+      res.status(400).json({
+        error: e
+      });
+    }
+
+    try {
       // Create a PaymentIntent on Stripe
       // A PaymentIntent represents your customer's intent to pay
       // and needs to be confirmed on the client to finalize the payment
@@ -170,20 +479,24 @@ module.exports = async (req, res) => {
     // For more information read https://stripe.com/docs/webhooks
     const sig = req.headers["stripe-signature"];
     let stripeEvent;
+    let metadata;
     try {
       // Verifies that the event was sent by Stripe and deserializes the event
-      stripeEvent = stripe.webhooks.constructEvent(
+      stripeEvent = await stripe.webhooks.constructEvent(
         req.body,
         sig,
         endpointSecret
       );
+      console.log("event", stripeEvent);
+
+      metadata = stripeEvent.data.object.metadata.order_id;
     } catch (err) {
       res.status(400).send(`Webhook Error: ${err.message}`);
     }
     // Handle the event
 
     /*const paymentIntent = stripeEvent.data.object;*/
-    const metadata = stripeEvent.data.object.metadata.order_id;
+
 
     switch (stripeEvent.type) {
       case "payment_intent.created":
