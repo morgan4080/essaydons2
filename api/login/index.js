efb3816031aa381697ef7347a8736ce17db8e0b9
morgan4080 <morganmurungi@live.com>
2021-10-18 01:36:08 +0300

    movetoxpress

--- api/login/index.js
+++ api/login/index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 
 const bodyParser = require('body-parser');
 
+const cors = require('cors')
+
+app.use(cors({}))
+
 app.use(bodyParser.json())
 
 import prisma from '../../lib/prisma'
